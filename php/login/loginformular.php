1ed57e33efb532f44991ba7f99bc3259d9756b35
flotitt <tittl.florian@gmail.com>
2025-12-17 18:08:07 +0100

    fjefrnöefgrnegro

--- php/login/loginformular.php
+++ php/login/loginformular.php
@@ -329,7 +329,7 @@
     </div>
   </main>
 
-  <?php include("/Webprojekt/php/include/footimport.php"); ?>
+  <?php include "/Webprojekt/php/include/footimport.php"; ?>
 
   <script>
     (function () {
